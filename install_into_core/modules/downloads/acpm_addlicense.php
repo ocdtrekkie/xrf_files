f27bbcd0e125e553812de9ce7bfe0e57ba8db417
Jacob Weisz <inbox@jacobweisz.com>
2019-01-18 00:05:54 -0600

    Upgrade control panels

--- install_into_core/modules/downloads/acpm_addlicense.php
+++ install_into_core/modules/downloads/acpm_addlicense.php
@@ -3,16 +3,16 @@ require("ismodule.php");
 
 if ($do == "generate")
 {
-	$customer = xrf_mysql_sanitize_string($_POST['customer']);
-	$assigned = xrf_mysql_sanitize_string($_POST['assigned']);
+	$customer = mysqli_real_escape_string($xrf_db, $_POST['customer']);
+	$assigned = mysqli_real_escape_string($xrf_db, $_POST['assigned']);
 	$pid = $_POST['pid'];
 	$pid = (int)$pid;
 
 	$query="SELECT * FROM g_users WHERE email='$customer' || id='$customer'";
-	$result=mysql_query($query);
-	@$custid=mysql_result($result,$i,"id");
-	@$email=mysql_result($result,$i,"email");
-	@$fname=mysql_result($result,$i,"fname");
+	$result=mysqli_query($xrf_db, $query);
+	@$custid=xrf_mysql_result($result,0,"id");
+	@$email=xrf_mysql_result($result,0,"email");
+	@$fname=xrf_mysql_result($result,0,"fname");
 
 	$key1 = xrf_generate_password(6);
 	$key2 = xrf_generate_password(7);
@@ -20,9 +20,9 @@ if ($do == "generate")
 	$lic_key = $pid . $key1 . "-" . $key2 . "-" . $key3;
 
 	$query="SELECT descr, lic_duration FROM d_products WHERE id='$pid'";
-	$result=mysql_query($query);
-	$product_name=mysql_result($result,$i,"descr");
-	$lic_duration=mysql_result($result,$i,"lic_duration");
+	$result=mysqli_query($xrf_db, $query);
+	$product_name=xrf_mysql_result($result,0,"descr");
+	$lic_duration=xrf_mysql_result($result,0,"lic_duration");
 	
 	if ($lic_duration == 0)
 	{
@@ -36,7 +36,7 @@ if ($do == "generate")
 		$expirymsg = " This license expires at $expiry.";
 	}
 	
-	mysql_query("INSERT INTO d_licenses (uid, pid, lic_key, assigned, expiry) VALUES('$custid', '$pid', '$lic_key', '$assigned', '$expiry')") or die(mysql_error()); 
+	mysqli_query($xrf_db, "INSERT INTO d_licenses (uid, pid, lic_key, assigned, expiry) VALUES('$custid', '$pid', '$lic_key', '$assigned', '$expiry')") or die(mysqli_error($xrf_db)); 
 
 	$from = "From: $xrf_admin_email \r\n";
 	mail($email, "$product_name License Key", "$fname,\n\nYou have been granted a license for $product_name for use on $assigned. Your license key is $lic_key.$expirymsg\n\nThank you,\n$xrf_site_name", $from);
@@ -52,12 +52,12 @@ else
 	<tr><td><b>Product:</b></td><td><select name=\"pid\">";
 
 	$query="SELECT * FROM d_products ORDER BY name, id ASC";
-	$result=mysql_query($query);
-	$num=mysql_num_rows($result);
+	$result=mysqli_query($xrf_db, $query);
+	$num=mysqli_num_rows($result);
 	$qq=0;
 	while ($qq < $num) {
-		$p_id=mysql_result($result,$qq,"id");
-		$p_descr=mysql_result($result,$qq,"descr");
+		$p_id=xrf_mysql_result($result,$qq,"id");
+		$p_descr=xrf_mysql_result($result,$qq,"descr");
 		echo "<option value='$p_id'>$p_descr</option>";
 		$qq++;
 	}
--- install_into_core/modules/downloads/m_viewlicenses.php
+++ install_into_core/modules/downloads/m_viewlicenses.php
@@ -3,8 +3,8 @@ require("ismodule.php");
 echo "<p><b>Your Purchased Licenses</b><p>";
 
 $query="SELECT * FROM d_licenses WHERE uid = $xrf_myid";
-$result=mysql_query($query);
-$num=mysql_num_rows($result);
+$result=mysqli_query($xrf_db, $query);
+$num=mysqli_num_rows($result);
 
 if ($num > 0)
 {
@@ -12,15 +12,15 @@ echo "<table><tr><td width=400><b>Product<br><i>Expiry</i></b><td width=400><b>L
 $qq=0;
 while ($qq < $num) {
 
-$id=mysql_result($result,$qq,"id");
-$pid=mysql_result($result,$qq,"pid");
-$lic_key=mysql_result($result,$qq,"lic_key");
-$assigned=mysql_result($result,$qq,"assigned");
-$expiry=mysql_result($result,$qq,"expiry");
+$id=xrf_mysql_result($result,$qq,"id");
+$pid=xrf_mysql_result($result,$qq,"pid");
+$lic_key=xrf_mysql_result($result,$qq,"lic_key");
+$assigned=xrf_mysql_result($result,$qq,"assigned");
+$expiry=xrf_mysql_result($result,$qq,"expiry");
 
 $pquery="SELECT descr FROM d_products WHERE id = '$pid'";
-$presult=mysql_query($pquery);
-$productname=mysql_result($presult,0,"descr");
+$presult=mysqli_query($xrf_db, $pquery);
+$productname=xrf_mysql_result($presult,0,"descr");
 
 if ($expiry == "0")
 	$expiry = "Never";
